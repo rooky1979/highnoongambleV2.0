74fdd1ea219c3f0dce9ef2ae810c24ec4304792b
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-02-07 15:26:02 +1300

    added JSON model for preamble

--- webapp/controller/Preamble.controller.js
+++ webapp/controller/Preamble.controller.js
@@ -1,30 +1,19 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller, JSONModel) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel) {
+    "use strict";
 
-        return Controller.extend("HNGv2.highnoongamble.controller.Preamble", {
-            onInit: function () {
-                
-                /* var path = jQuery.sap.getModulePath("HNGv2.highnoongamble", "webapp/model/preamble.json");
-
-                var model = new JSONModel(path);
-
-                this.getView().setModel(model, "preamble");  */
-                /*this below code for get the JSON Model form Manifest.json file*/
-			var dataModel = this.getOwnerComponent().getModel("preamble");
-			this.getView().setModel(dataModel, "preamble");
-
-            },
-            pressTile: function (oEvent) {
-                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                oRouter.navTo("preamble");
-            }
-        });
+    return Controller.extend("HNGv2.highnoongamble.controller.Preamble", {
+      onInit: function () {
+        var preamble = new sap.ui.model.json.JSONModel(
+          "model/preamble.json"
+        );
+        this.getView().setModel(preamble, "preamble");
+      },
     });
-    //GO BACK BUTTON
+  }
+);
+//GO BACK BUTTON
