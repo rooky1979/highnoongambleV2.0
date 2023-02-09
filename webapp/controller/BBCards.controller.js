d042b1cfc1f55b9b9fdee39b9ec5250502dbc459
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-02-09 15:43:53 +1300

    Added bbcards table and started search bar filter

--- webapp/controller/BBCards.controller.js
+++ webapp/controller/BBCards.controller.js
@@ -1,18 +1,40 @@
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+    ["sap/ui/core/mvc/Controller", "sap/ui/model/Filter", "sap/ui/model/FilterOperator"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, Filter, FilterOperator) {
       "use strict";
   
       return Controller.extend("HNGv2.highnoongamble.controller.BBCards", {
         onInit: function () {
-          var bbcards = new sap.ui.model.json.JSONModel(
-            "model/bbcards.json"
-          );
-          this.getView().setModel(bbcards, "bbcards");
+          var bbcardTable = this.getView().byId("bbcards")
+          var bbcardsModel = new sap.ui.model.json.JSONModel();
+          bbcardsModel.loadData("model/bbcards.json")
+          bbcardTable.setModel(bbcardsModel);
+          //this.getView().setModel(bbcards, "bbcards");
         },
+        onSearch: function(oEvent) {
+          /* var filters = [];
+          var query = event.getSource().getValue();
+          if(query && query.length > 0) {
+            var filter = new Filter("id", sap.ui.FilterOperator.Contains, query);
+            filters.push(filter);
+          }
+          var list = this.getView().byId("bbcards");
+          var binding = list.getBinding("items");
+          binding.filter(filters, "application"); */
+          var aFilter = [];
+          var sQuery = oEvent.getParameter("query");
+          if (sQuery) {
+              aFilter.push(new Filter("id", FilterOperator.Contains, sQuery));
+          }
+
+          // filter binding
+          var oList = this.byId("bbcards");
+          var oBinding = oList.getBinding("items");
+          oBinding.filter(aFilter);
+        }
       });
     }
   );
